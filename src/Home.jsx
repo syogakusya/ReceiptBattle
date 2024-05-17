6d0b489b4f2c75e4734c37a4f5a1b7072337704f
yoyo <hsjejxys@gmail.com>
2024-05-17 19:58:16 +0900

    ユーザー情報をDBから取得して表示

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { auth, provider } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from './firebase';
 
 function Home() {
   const [user] = useAuthState(auth);
@@ -21,7 +24,7 @@ function Home() {
   );
 }
 
-export default Home;
+export default Home; // Homeをデフォルトエクスポート
 
 function SignInButton() {
   const signInWithGoogle = () => {
@@ -53,11 +56,53 @@ function UserInfo() {
 }
 
 function Start() {
+  const userInfo = useFetchCurrentUser(); // ユーザー情報をフックから取得
+
   return (
-    <button>
-      <Link to={`/top`}>
-        <p>スタート</p>
-      </Link>
-    </button>
+    <div>
+      {userInfo ? (
+        // ユーザーが見つかった場合の表示
+        <button>
+          <Link to={`/top`}>
+            <p>スタート</p>
+          </Link>
+        </button>
+      ) : (
+        // ユーザー情報がない場合の表示
+        <div>
+          <p>ユーザー情報を取得中...</p>
+        </div>
+      )}
+    </div>
   );
+}
+
+export function useFetchCurrentUser() {
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchUsersData = async () => {
+      try {
+        const usersData = collection(db, 'users');
+        const snapShot = await getDocs(usersData);
+        const usersFetched = snapShot.docs.map((doc) => ({ ...doc.data() }));
+        const currentUserId = auth.currentUser.uid;
+        const foundIndex = usersFetched.findIndex(
+          (p) => p.userID === currentUserId
+        );
+
+        if (foundIndex !== -1) {
+          setUserInfo(usersFetched[foundIndex]);
+        } else {
+          console.log('登録されていません');
+        }
+      } catch (error) {
+        console.error('Error fetching user data: ', error);
+      }
+    };
+
+    fetchUsersData();
+  }, []);
+
+  return userInfo;
 }

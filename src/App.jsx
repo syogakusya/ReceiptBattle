ec53768674adde3e50c1aa7742449ebba876e060
yoyo <hsjejxys@gmail.com>
2024-05-17 16:10:26 +0900

    ルーティング設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Users from './Users';
+import Top from './Top';
 
 function App() {
   return (
     <div className="App">
-      <h1>Firebaseを使ってログイン機能を実装</h1>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/top" element={<Top />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }

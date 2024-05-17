165209876a9af42eb120abf07a4e66b338800f38
yoyo <hsjejxys@gmail.com>
2024-05-18 01:08:51 +0900

    ユーザの武器を表示する

--- src/Top.jsx
+++ src/Top.jsx
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { auth } from './firebase';
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { Link } from 'react-router-dom';
-import { useFetchCurrentUser } from './Home'; // ここでカスタムフックをインポートします。パスは適切に修正してください。
+import { useFetchCurrentUser } from './Home'; 
+import './App.css';
 
 function Top() {
   const [uid, setUid] = useState(null);
-  const currentUserInfo = useFetchCurrentUser(); // userInfo が変数名として既に存在しているため、ここでは currentUserInfo という名前を使います
-  console.log(currentUserInfo);
+  const [imageUrl, setImageUrl] = useState(''); 
+  const currentUserInfo = useFetchCurrentUser(); 
 
   useEffect(() => {
-    if (auth.currentUser) {
+    if (auth.currentUser && currentUserInfo && currentUserInfo.weaponID) {
       setUid(auth.currentUser.uid);
       console.log(auth.currentUser.uid);
+
+      const storage = getStorage();
+      const imageName = `weapon${currentUserInfo.weaponID}.png`;
+      const imageRef = ref(storage, `weapon/${imageName}`); 
+
+      getDownloadURL(imageRef)
+        .then((url) => {
+          setImageUrl(url); 
+        })
+        .catch((error) => {
+          console.error("画像の取得に失敗しました:", error);
+        });
     }
-  }, []);
+  }, [currentUserInfo]); 
 
   return (
     <>
@@ -24,6 +38,7 @@ function Top() {
           <div>ユーザー名: {currentUserInfo.userName}</div>
         </>
       )}
+      {imageUrl && <img className="weapon_img" src={imageUrl} alt={`Weapon ${currentUserInfo?.weaponID}`} />} 
       <button>
         <Link to={`/users`}>
           <p>ユーザー一覧</p>

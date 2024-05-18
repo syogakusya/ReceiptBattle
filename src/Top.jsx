a008d5ecdc4131bfb4e37daf7e20d8b9fb1ad77a
yoyo <hsjejxys@gmail.com>
2024-05-18 16:08:36 +0900

    キャラクター画像を表示

--- src/Top.jsx
+++ src/Top.jsx
@@ -8,24 +8,34 @@ import './App.css';
 function Top() {
   const [uid, setUid] = useState(null);
   const [weaponUrl, setWeaponUrl] = useState('');
+  const [characterUrl, setCharacterURL] = useState('');
   const currentUserInfo = useFetchCurrentUser(); 
 
   useEffect(() => {
-    if (auth.currentUser && currentUserInfo && currentUserInfo.weaponID) {
+    if (auth.currentUser && currentUserInfo) {
       setUid(auth.currentUser.uid);
       console.log(auth.currentUser.uid);
 
       const storage = getStorage();
       const weaponName = `weapon${currentUserInfo.weaponID}.png`;
-      const weaponRef = ref(storage, `weapon/${weaponName}`); 
+      const weaponRef = ref(storage, `weapon/${weaponName}`);
+      const characterName = `character${currentUserInfo.characterID}.png`;
+      const characterRef = ref(storage, `character/${characterName}`);
 
       getDownloadURL(weaponRef)
         .then((url) => {
           setWeaponUrl(url); 
         })
         .catch((error) => {
-          console.error("画像の取得に失敗しました:", error);
+          console.error("武器画像の取得に失敗しました:", error);
         });
+      getDownloadURL(characterRef)
+        .then((url) => {
+          setCharacterURL(url);
+        })
+        .catch((error) => {
+          console.error("キャラクター画像の取得に失敗しました:", error);
+        })
     }
   }, [currentUserInfo]); 
 
@@ -38,7 +48,8 @@ function Top() {
           <div>ユーザー名: {currentUserInfo.userName}</div>
         </>
       )}
-      {weaponUrl && <img className="weapon_img" src={weaponUrl} alt={`Weapon ${currentUserInfo?.weaponID}`} />} 
+      {weaponUrl && <img className="weapon_img" src={weaponUrl} alt={`Weapon ${currentUserInfo?.weaponID}`} />}
+      {characterUrl && <img className="character_img" src={characterUrl} alt={`Character ${currentUserInfo?.characterID}`} />}  
       <button>
         <Link to={`/users`}>
           <p>ユーザー一覧</p>

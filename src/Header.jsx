bf658cc37ef2a24fa330f199e0314c925b5ed9fc
DaikiKakimi <4250ikiad@gmail.com>
2024-05-21 16:49:17 +0900

    ボタン修正

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,10 @@ function Header() {
     return (
       <div className="m-8">
         <Link to={'/top'}>
-          <img src="../src/images/Home.png" className="text-black w-8 h-8" />
+          <img
+            src="../src/images/Home.png"
+            className="text-black w-[46.88px] h-[40.62]"
+          />
         </Link>
       </div>
     );
